58ad35390b83330e0ce7a67ce0b22303314dfee8
Venkat Akhil <81106437+venkat-akhil-beesetty@users.noreply.github.com>
2022-01-26 16:05:17 +0530

    Delete sdp directory

--- sdp/project/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from .models import Feedback, NormalPay,profile
-
-admin.site.register(Feedback)
-admin.site.register(NormalPay)
-admin.site.register(profile)
